7df3257c2a8240d1558ce0b7817943a731d838bf
Diego Sousa <derickbenji2@gmail.com>
2022-11-25 23:40:17 -0300

    finalizando setup inicial mobile

---
+++ src/screens/Signin.tsx
@@ -0,0 +1,11 @@
+import { Center, Text } from "native-base";
+
+export function Signin() {
+  return (
+    <Center flex={1} bgColor="gray.800">
+      <Text color="white" fontFamily="heading">
+        Signin
+      </Text>
+    </Center>
+  );
+}
